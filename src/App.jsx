dac1199a1772fc55fd148588b356704baa943455
goswamiArnab03 <arnabgoswami812@gmail.com>
2025-04-11 01:41:09 +0530

    ToDoAppReact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import Navber from './components/Navber'
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 function App() {
   const [todo, setTodo] = useState("")
@@ -12,7 +15,7 @@ function App() {
 
   }
   const h_add=()=>{
-    setTodos([...todos,{todo,isCompleted:false}])
+    setTodos([...todos,{id:uuidv4(),todo,isCompleted:false}])
     setTodo("")
   }
   const h_edit=()=>{
@@ -20,6 +23,17 @@ function App() {
   }
   const h_change=(e)=>{
     setTodo(e.target.value)
+  }
+
+  const h_checkbox=(e)=>{
+    let id=e.target.name
+    let ind=todos.findIndex(item=>{
+      return item.id===id;
+    })
+    let newTodos=[...todos];
+    newTodos[ind].isCompleted=!newTodos[ind].isCompleted;
+    setTodos(newTodos);
+
   }
 
   return (
@@ -37,8 +51,12 @@ function App() {
       <h2 className='font-bold text-lg  underline pb-5 '>Your Todos</h2>
       <div className="todos">
         {todos.map(item=>{
-          return <div key={todo} className='todo flex justify-between my-3'>
-            <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+          return <div key={item.id} className='todo flex justify-between my-3'>
+            
+            <div className={item.isCompleted ? "line-through" : ""}>
+            <input type="checkbox" onChange={h_checkbox} value={item.isCompleted} name={item.id} id="" />
+            <span className='px-2'>{item.todo}</span>
+            </div>
             <div className="buttons">
             <button onClick={h_edit} className='bg-purple-700 hover:bg-purple-800 cursor-pointer p-3 rounded-xl py-1 text-white transition-none'>Edit</button>
             <button onClick={h_delete} className='bg-purple-700 hover:bg-purple-800 cursor-pointer p-3 rounded-xl py-1 text-white transition-none'>Delete</button>
